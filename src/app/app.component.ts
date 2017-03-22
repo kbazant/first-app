1b32231a0959cfee17bb932a128d85a29d3b5a18
kbazant <krizz100@yahoo.com>
2017-03-22 08:18:26 +0000

    replaced links to html and css file with html code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+	template: `
+		<h1>Inline Template</h1>	
+`,
 export class AppComponent {
   title = 'I  changed it!';
 }
