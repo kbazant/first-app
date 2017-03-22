fdcdd8365bf10f31b4e0c086917fe64e1df82f8b
kbazant <krizz100@yahoo.com>
2017-03-22 08:46:47 +0000

    removed link to other.component.css file

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-other',
-  templateUrl: './other.component.html',
-  styleUrls: ['./other.component.css']
+  templateUrl: './other.component.html'
 })
 export class OtherComponent implements OnInit {
 
